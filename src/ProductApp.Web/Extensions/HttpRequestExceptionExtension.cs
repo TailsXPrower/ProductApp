0534e6ffff2c7ab0d8d3b4ee2b43f8dc8ffbfd34
TailsXPrower <Dj.Delssin@gmail.com>
2026-02-13 05:43:00 +0200

    Added Error Handling

---
+++ src/ProductApp.Web/Extensions/HttpRequestExceptionExtension.cs
@@ -0,0 +1,47 @@
+using System.Text;
+using System.Text.Json;
+
+namespace ProductApp.Web.Exceptions;
+
+public static class HttpRequestExceptionExtension
+{
+    public static string BuildErrorMessage(this HttpRequestException httpException)
+    {
+        const string defaultMessage = "Request failed.";
+
+        if (string.IsNullOrWhiteSpace(httpException.Message))
+        {
+            return defaultMessage;
+        }
+
+        try
+        {
+            var document = JsonDocument.Parse(httpException.Message);
+            var root = document.RootElement;
+
+            var builder = new StringBuilder();
+            if (root.TryGetProperty("message", out var message))
+            {
+                builder.Append(message.GetString());
+            }
+
+            if (!root.TryGetProperty("errors", out var errors) || errors.ValueKind != JsonValueKind.Object) return builder.Length > 0 ? builder.ToString() : httpException.Message;
+            
+            foreach (var errorMessage in errors.EnumerateObject().SelectMany(property => property.Value.EnumerateArray()))
+            {
+                if (builder.Length > 0)
+                {
+                    builder.Append(' ');
+                }
+
+                builder.Append(errorMessage.GetString());
+            }
+
+            return builder.Length > 0 ? builder.ToString() : httpException.Message;
+        }
+        catch (JsonException)
+        {
+            return httpException.Message;
+        }
+    }
+}
